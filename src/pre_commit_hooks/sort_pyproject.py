0fa58a7e856695bd2d5b8bff74d34e1a9cd52f28
Boris Pfahringer <pfahringer@dhzb.de>
2022-10-21 17:56:31 +0200

    feat: add support for dep groups

--- src/pre_commit_hooks/sort_pyproject.py
+++ src/pre_commit_hooks/sort_pyproject.py
@@ -3,35 +3,41 @@
 import tomlkit
 
 
-def main():
-    with open("pyproject.toml") as f:
-        pyproj = tomlkit.load(f)
-
-    deps = pyproj["tool"]["poetry"]["dependencies"]
-    dev_deps = pyproj["tool"]["poetry"]["dev-dependencies"]
-
+def sort_deps(poetry: tomlkit.table, *args: list[str]) -> tomlkit.table:
     deps_sorted = tomlkit.table()
-    dev_deps_sorted = tomlkit.table()
 
-    deps_sorted["python"] = deps["python"]
-    del deps["python"]
+    deps = poetry
+    for item in args:
+        deps = deps[item]
 
-    key_fn = lambda kv: kv[0].lower()
-
-    for key, value in sorted(deps.items(), key=key_fn):
+    for key, value in sorted(deps.items(), key=lambda kv: kv[0].lower()):
         deps_sorted.append(key, value)
-
-    for key, value in sorted(dev_deps.items(), key=key_fn):
-        dev_deps_sorted.append(key, value)
 
     if deps.value.body[-1][0] is None:
         deps_sorted.add(deps.value.body[-1][1])
 
-    if dev_deps.value.body[-1][0] is None:
-        dev_deps_sorted.add(dev_deps.value.body[-1][1])
+    return deps_sorted
 
-    pyproj["tool"]["poetry"]["dependencies"] = deps_sorted
-    pyproj["tool"]["poetry"]["dev-dependencies"] = dev_deps_sorted
+
+def main():
+    with open("pyproject.toml") as f:
+        pyproj = tomlkit.load(f)
+
+    poetry = pyproj["tool"]["poetry"]
+
+    deps_sorted = tomlkit.table()
+    deps_sorted["python"] = poetry["dependencies"].pop("python")
+    deps_sorted.update(sort_deps(poetry, "dependencies"))
+    poetry["dependencies"] = deps_sorted
+
+    if "dev-dependencies" in poetry:
+        poetry["dev-dependencies"] = sort_deps(poetry, "dev-dependencies")
+
+    if "group" in poetry:
+        for group in poetry["group"]:
+            poetry["group"][group]["dependencies"] = sort_deps(
+                poetry, "group", group, "dependencies"
+            )
 
     with open("pyproject.toml", "w") as f:
         tomlkit.dump(pyproj, f)
